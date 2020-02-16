512ef5557ced78417d060a6d86af5514e5cdbd37
Albert Felix <59144143+albert-felix@users.noreply.github.com>
2020-02-16 11:40:27 +0530

    bootstrap-handlebars
    used bootstrap and handlebars to create students website

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
+const path =  require("path");
 const studentsRouter = require("./Router/studentsRouter");
 const studentRouter = require("./Router/studentRouter");
 
 const app = express();
+
+const hbs = expressHbs.create({
+  extname : ".hbs",
+  layoutsDir : path.join(__dirname,"./views/layouts"),
+  partialsDir : path.join(__dirname,"./views/partials")
+});
+
+app.engine(".hbs",hbs.engine);
+app.set("view engine",".hbs");
+app.set("views",path.join(__dirname,"./views"));
 
 app.use(bodyParser.json());
 
@@ -14,7 +26,9 @@ app.use(bodyParser.json());
 // });
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.render("home",{
+    layout: "hero"
+  });
 });
 
 app.use("/students", studentsRouter);
